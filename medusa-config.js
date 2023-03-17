b1ae12c81c092f148a7d7a16ace076efbe75446e
Farhan <farhan.farooq@live.com>
2023-03-17 13:26:48 +0500

    initial

--- medusa-config.js
+++ medusa-config.js
@@ -65,8 +65,8 @@ module.exports = {
     cookie_secret: 'cookieeeee',
     // database_database: "./medusa-db.sql",
     // database_type: "sqlite",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
+    // store_cors: STORE_CORS,
+    // admin_cors: ADMIN_CORS,
   },
   plugins,
 };
